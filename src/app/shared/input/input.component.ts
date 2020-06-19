feabce2ad71b2a27dfb29f83403f9d7ff534826c
kpeter74 <kisvaradi.peter@gmail.com>
2020-06-19 12:18:24 +0200

    reusable input component

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
+  @Input() label: string;
+  @Input() control: FormControl;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
